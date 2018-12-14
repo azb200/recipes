441b701163f8ad0d7ba7223da945b4aa7e788ce0
gabriel <azb200@yahoo.com>
2018-12-13 23:44:40 -0500

    Updates

---
+++ precache-manifest.3432ba21800b6a6c2178fb38a2fc3f2a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/recipes/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "99f103c5410eb7b9e495",
+    "url": "/recipes/static/css/main.f6feed80.chunk.css"
+  },
+  {
+    "revision": "c96165c226ec31117b63d29641a80727",
+    "url": "/recipes/static/media/banner.c96165c2.png"
+  },
+  {
+    "revision": "f78c872982d091a27357",
+    "url": "/recipes/static/js/1.f78c8729.chunk.js"
+  },
+  {
+    "revision": "aa6e30bfc1d295068424",
+    "url": "/recipes/static/js/runtime~main.aa6e30bf.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/recipes/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "99f103c5410eb7b9e495",
+    "url": "/recipes/static/js/main.99f103c5.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/recipes/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/recipes/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/recipes/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "f78c872982d091a27357",
+    "url": "/recipes/static/css/1.af349c87.chunk.css"
+  },
+  {
+    "revision": "ec116b71f04ea01da86a5c21e215a122",
+    "url": "/recipes/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/food/precache-manifest.d89fa0a6a7e63b6e66d4932dd78d9836.js"
+  "/recipes/precache-manifest.3432ba21800b6a6c2178fb38a2fc3f2a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/food/index.html", {
+workbox.routing.registerNavigationRoute("/recipes/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });

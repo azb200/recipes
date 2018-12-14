c609cb7eef842e0386eb72209c0444a12b902b2a
gabriel <azb200@yahoo.com>
2018-12-13 23:38:33 -0500

    Updates

---
+++ precache-manifest.d89fa0a6a7e63b6e66d4932dd78d9836.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/food/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "c0c810fb88d77ff34714",
+    "url": "/food/static/css/main.d1e4a57d.chunk.css"
+  },
+  {
+    "revision": "c96165c226ec31117b63d29641a80727",
+    "url": "/food/static/media/banner.c96165c2.png"
+  },
+  {
+    "revision": "3f31a0b0b04d225a3555",
+    "url": "/food/static/js/1.3f31a0b0.chunk.js"
+  },
+  {
+    "revision": "67fe68913997935f0b4c",
+    "url": "/food/static/js/runtime~main.67fe6891.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/food/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "c0c810fb88d77ff34714",
+    "url": "/food/static/js/main.c0c810fb.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/food/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/food/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/food/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "3f31a0b0b04d225a3555",
+    "url": "/food/static/css/1.49a7d8d5.chunk.css"
+  },
+  {
+    "revision": "b4be8ac76e9f8eb7f6371773ebed6b4a",
+    "url": "/food/index.html"
+  }
+];
